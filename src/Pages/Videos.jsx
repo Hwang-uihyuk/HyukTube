fc614e1d93a66325fc518b6e6de8deb5576c5d01
Hwang-uihyuk <weihyuk39@gmail.com>
2023-03-15 09:37:55 +0900

    feat : fakeyoubueclient 걷고 실제 youtubeclient 및 데이터 캐시 stale 작업

--- src/Pages/Videos.jsx
+++ src/Pages/Videos.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
-
-import Youtube, { search } from '../api/youtube';
-import FakeYoutube from '../api/fakeYotubeClient';
 import { useYoutubeApi } from '../context/YoutubeApiContext';
 import VideoCard from '../Components/VideoCard';
 
@@ -14,7 +11,9 @@ export default function Videos() {
     isLoading,
     error,
     data: videos,
-  } = useQuery(['videos', keyword], () => youtube.search(keyword));
+  } = useQuery(['videos', keyword], () => youtube.search(keyword), {
+    staleTime : 1000 * 60 * 1,
+  });
 
   return (
     <>
